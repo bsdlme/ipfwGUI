4b95351f5230317dfab164b191812b8632c4dac4
Lars Engels <lme@FreeBSD.org>
2025-03-12 21:19:14 +0100

    Rework ipfwGUI
    Use "sockstat -46s" and don't parse it with awk / sed, etc. With -s we
    can only see real listening sockets which we can offer to allow / deny
    in ipfwGUI.

--- ipfwGUI.py
+++ ipfwGUI.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Copyright (c) 2020 Lars Engels. All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
@@ -26,18 +26,21 @@ import os
 import sys
 import re
 
-from PyQt6.QtWidgets import (
-    QApplication, QMainWindow, QMessageBox, QWidget, QFormLayout, QCheckBox,
-    QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem,
-    QStatusBar, QGridLayout, QStyle)
+from subprocess import check_output, CalledProcessError
 from PyQt6.QtCore import Qt
-from subprocess import check_output
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QMessageBox, QWidget, QGridLayout,
+    QCheckBox, QLabel, QPushButton, QTableWidget, QTableWidgetItem,
+    QStatusBar, QStyle
+)
 
+# Constants for system binaries
 SYSRC_BIN = '/usr/sbin/sysrc'
 SERVICE_BIN = '/usr/sbin/service'
 SOCKSTAT_BIN = '/usr/bin/sockstat'
 
-class simpleIpfwGui(QMainWindow):
+
+class SimpleIpfwGui(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Simple Firewall GUI for FreeBSD")
@@ -47,27 +50,19 @@ class simpleIpfwGui(QMainWindow):
         self.layout = QGridLayout()
         self.centralWidget.setLayout(self.layout)
 
-        # firewallEnabled: firewall_enabled=YES in rc.conf, firewallRunningString: ipfw loaded and running
-        (self.firewallEnabled, self.firewallRunningString, self.firewallRunningBool) = self.getFirewallState()
+        self.firewallEnabled, self.firewallRunningString, self.firewallRunningBool = self.getFirewallState()
         self.allowedPorts = self.getAllowedPorts()
 
-        self.widgets()
+        self.setupWidgets()
+        self.checkPrivileges()
 
-    def widgets(self):
+    def setupWidgets(self):
         self.labelTitle = QLabel("Simple Firewall GUI for FreeBSD")
         self.labelTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.labelcheckBoxIpfwEnable = QLabel("Enable Firewall? ")
-        #self.labelAllowedPorts = QLabel("Allowed incoming ports:")
-
         self.checkBoxIpfwEnable = QCheckBox()
         self.checkBoxIpfwEnable.setToolTip('Check this to enable the firewall.')
-        if self.firewallEnabled.lower() == "yes":
-            self.checkBoxIpfwEnable.setChecked(True)
-
-        #self.editAllowedPorts = QLineEdit()
-        #self.editAllowedPorts.setFixedWidth(500)
-        #self.editAllowedPorts.setText(' '.join(self.allowedPorts))
+        self.checkBoxIpfwEnable.setChecked(self.firewallEnabled.lower() == "yes")
 
         self.buttonApply = QPushButton("Apply")
         self.buttonApply.setFixedWidth(120)
@@ -77,10 +72,6 @@ class simpleIpfwGui(QMainWindow):
         self.buttonQuit.setFixedWidth(120)
         self.buttonQuit.clicked.connect(QApplication.instance().quit)
 
-        #self.buttonClear = QPushButton("Clear")
-        #self.buttonClear.setFixedWidth(120)
-        #self.buttonClear.clicked.connect(self.clearAllowedPorts)
-
         self.createTable()
 
         self.statusBar = QStatusBar()
@@ -88,154 +79,129 @@ class simpleIpfwGui(QMainWindow):
         self.updateStatusBar()
         self.setStatusBar(self.statusBar)
 
-        self.layout.addWidget(self.labelTitle,              0, 1)
-
-        self.layout.addWidget(self.labelcheckBoxIpfwEnable, 1, 0)
-        self.layout.addWidget(self.checkBoxIpfwEnable,      1, 1)
-
+        self.layout.addWidget(self.labelTitle, 0, 1)
+        self.layout.addWidget(QLabel("Enable Firewall? "), 1, 0)
+        self.layout.addWidget(self.checkBoxIpfwEnable, 1, 1)
         self.layout.addWidget(self.tableWidget, 2, 0, 1, 3)
-
-        #self.layout.addWidget(self.labelAllowedPorts,       3, 0)
-        #self.layout.addWidget(self.editAllowedPorts,        4, 0, 1, 3)
-
-        #self.layout.addWidget(self.buttonClear,              5, 0)
-        self.layout.addWidget(self.buttonApply,             5, 1, alignment=Qt.AlignmentFlag.AlignRight)
-        self.layout.addWidget(self.buttonQuit,              5, 2)
-
-        #self.sanitizeInput(self.editAllowedPorts.text())
-        self.checkPrivileges()
+        self.layout.addWidget(self.buttonApply, 5, 1, alignment=Qt.AlignmentFlag.AlignRight)
+        self.layout.addWidget(self.buttonQuit, 5, 2)
 
     def createTable(self):
         self.tableWidget = QTableWidget()
-        self.tableContent = self.getListenPorts().strip().decode("utf-8")
+        self.tableContent = self.getListenPorts()
 
-        self.tableWidget.setRowCount(self.tableContent.count("\n"))
+        self.tableWidget.setRowCount(len(self.tableContent))
         self.tableWidget.setColumnCount(4)
         self.tableWidget.setHorizontalHeaderLabels(["Process", "Protocol", "Port", "Allow"])
-        self.lineNum = 0
-        for line in self.tableContent.split("\n"):
-            (self.proc, self.proto, self.port) = line.split()
-            self.tableWidget.setItem(self.lineNum, 0, QTableWidgetItem(self.proc))
-            self.tableWidget.setItem(self.lineNum, 1, QTableWidgetItem(self.proto))
-            self.tableWidget.setItem(self.lineNum, 2, QTableWidgetItem(self.port))
-            self.checkbox = QTableWidgetItem()
-            self.checkbox.setFlags(self.checkbox.flags() | Qt.ItemFlag.ItemIsUserCheckable)
-            if str(self.port) + "/" + self.proto.rstrip("46") in self.allowedPorts:
-                self.checkbox.setCheckState(Qt.CheckState.Checked)
-            else:
-                self.checkbox.setCheckState(Qt.CheckState.Unchecked)
-            self.tableWidget.setItem(self.lineNum, 3, self.checkbox)
-            self.lineNum += 1
-        self.tableWidget.move(0,0)
+
+        for lineNum, line in enumerate(self.tableContent):
+            proc, proto, port = line
+            self.tableWidget.setItem(lineNum, 0, QTableWidgetItem(proc))
+            self.tableWidget.setItem(lineNum, 1, QTableWidgetItem(proto))
+            self.tableWidget.setItem(lineNum, 2, QTableWidgetItem(port))
+
+            checkbox = QTableWidgetItem()
+            checkbox.setFlags(checkbox.flags() | Qt.ItemFlag.ItemIsUserCheckable)
+            checkbox.setCheckState(Qt.CheckState.Checked if f"{port}/{proto.rstrip('46')}" in self.allowedPorts else Qt.CheckState.Unchecked)
+            self.tableWidget.setItem(lineNum, 3, checkbox)
 
     def applyChanges(self):
         if not self.checkPrivileges():
             return
-        if self.checkBoxIpfwEnable.isChecked() == True:
-            self.fwEnable = "YES"
-            self.serviceAction = "start"
-        else:
-            self.fwEnable = "NO"
-            self.serviceAction = "onestop"
 
-        # Add enabled checkboxes to the list
-        items = []
-        for i in range(self.tableWidget.rowCount()):
-            item = self.tableWidget.item(i, 3)
-            if item.checkState() == Qt.CheckState.Checked:
-                items.append(item)
+        self.fwEnable = "YES" if self.checkBoxIpfwEnable.isChecked() else "NO"
+        self.serviceAction = "start" if self.fwEnable == "YES" else "onestop"
 
-        self.allowedPortsNew = []
-        for it in items:
-            r = it.row()
-            portProto = self.tableWidget.item(r, 2).text() + "/" + self.tableWidget.item(r, 1).text().rstrip('46')
-            self.allowedPortsNew.append(portProto)
+        allowedPortsNew = [
+            f"{self.tableWidget.item(i, 2).text()}/{self.tableWidget.item(i, 1).text().rstrip('46')}"
+            for i in range(self.tableWidget.rowCount())
+            if self.tableWidget.item(i, 3).checkState() == Qt.CheckState.Checked
+        ]
 
-        #self.editAllowedPorts.setText(' '.join(self.allowedPorts))
-        #self.sanitizeInput(self.editAllowedPorts.text())
 
-        self.allowedPortsNew = sorted(self.allowedPortsNew, key=natural_keys)
+        allowedPortsNew = sorted(set(allowedPortsNew), key=self.natural_keys)
 
-        print(check_output([SYSRC_BIN, 'firewall_enable=' + self.fwEnable]).rstrip().decode("utf-8"))
-        print(check_output([SYSRC_BIN, 'firewall_type=workstation']).rstrip().decode("utf-8"))
-        print(check_output([SYSRC_BIN, 'firewall_allowservices=any']).rstrip().decode("utf-8"))
-        #print(check_output([SYSRC_BIN, 'firewall_myservices=' + ' '.join(self.editAllowedPorts.text().strip())].decode("utf-8")))
-        print(check_output([SYSRC_BIN, 'firewall_myservices=' + ' '.join(self.allowedPortsNew)]))
-        print(check_output([SERVICE_BIN, 'ipfw', self.serviceAction]))
-        (self.firewallEnabled, self.firewallRunningString, self.firewallRunningBool) = self.getFirewallState()
+        try:
+            check_output([SYSRC_BIN, f'firewall_enable={self.fwEnable}'])
+            check_output([SYSRC_BIN, 'firewall_type=workstation'])
+            check_output([SYSRC_BIN, 'firewall_allowservices=any'])
+            check_output([SYSRC_BIN, f'firewall_myservices={" ".join(allowedPortsNew)}'])
+            check_output([SERVICE_BIN, 'ipfw', self.serviceAction])
+        except CalledProcessError as e:
+            QMessageBox.critical(self, "Error", f"Failed to apply changes: {e}")
+            return
+
+        self.firewallEnabled, self.firewallRunningString, self.firewallRunningBool = self.getFirewallState()
         self.updateStatusBar()
 
     def getFirewallState(self):
-        self.firewallEnabled = check_output([SYSRC_BIN, '-n', 'firewall_enable']).rstrip().decode("utf-8")
-        self.firewallRunningString = check_output(SERVICE_BIN + ' ipfw forcestatus; exit 0', shell=True).rstrip().decode("utf-8")
-        if self.firewallRunningString == "ipfw is enabled":
-            self.firewallRunningBool = True
-        else:
-            self.firewallRunningBool = False
-        self.firewallRunningString = self.firewallRunningString.replace('enabled', 'running')
-        self.firewallRunningString = self.firewallRunningString.replace('ipfw', 'Firewall')
-        return (self.firewallEnabled, self.firewallRunningString, self.firewallRunningBool)
+        try:
+            firewallEnabled = check_output([SYSRC_BIN, '-n', 'firewall_enable']).strip().decode("utf-8")
+            firewallRunningString = check_output(f"{SERVICE_BIN} ipfw forcestatus; exit 0", shell=True).strip().decode("utf-8")
+            firewallRunningBool = firewallRunningString == "ipfw is enabled"
+            firewallRunningString = firewallRunningString.replace('enabled', 'running').replace('ipfw', 'Firewall')
+            return firewallEnabled, firewallRunningString, firewallRunningBool
+        except CalledProcessError:
+            return "NO", "Firewall status unknown", False
 
     def getAllowedPorts(self):
-        return (check_output([SYSRC_BIN, '-n', 'firewall_myservices'])).strip().decode("utf-8").split(" ")
-
-    #def clearAllowedPorts(self):
-    #    self.editAllowedPorts.setText('')
-    #    return
-
-    #def updateAllowedPorts(self, state):
-    #    if state == Qt.Checked:
-    #    else:
-    #    
-    #    self.sanitizeInput(self.editAllowedPorts.text())
-    #    return
+        try:
+            return check_output([SYSRC_BIN, '-n', 'firewall_myservices']).strip().decode("utf-8").split()
+        except CalledProcessError:
+            return []
 
     def checkPrivileges(self):
         if os.geteuid() != 0:
             QMessageBox.information(self, "Not enough privileges",
               "You started the firewall management GUI as a regular user and can only read the firewall settings.\n\nIf you want to edit settings, run the firewall management GUI as root.")
             return False
-        else:
-            return True
+        return True
 
     def updateStatusBar(self):
-        if self.firewallRunningBool == True:
-            self.statusBar.setStyleSheet("background-color : lightgreen") 
-        else:
-            self.statusBar.setStyleSheet("background-color : pink") 
+        self.statusBar.setStyleSheet("background-color : lightgreen" if self.firewallRunningBool else "background-color : pink")
         self.statusBar.showMessage(self.firewallRunningString)
 
     def getListenPorts(self):
-        return (check_output(SOCKSTAT_BIN + ' -46lLwqj0 | tr -s " " | cut -d " " -f 2,5,6 | awk -F\'[ :]\' \'{sub(/\*/, "", $1);sub(/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/, "", $1); conn[$NF] = sprintf("%s %s", $1,$2)} END{for (port in conn){ print conn[port], port}}\' | sort -nk3 | sed \'/\*$/d\'', shell=True))
+        try:
+            # Get the output from sockstat
+            sockstat_output = check_output([SOCKSTAT_BIN, '-46s']).decode('utf-8')
 
-    #def sanitizeInput(self, allowedPorts):
-    #    tmpArray = []
-    #    for elem in self.editAllowedPorts.text().split(" "):
-    #        tmpArray.append(elem.strip(' ,;:\t'))
+            # Process the output
+            lines = sockstat_output.strip().splitlines()
+            connections = []
 
-    #    tmpArray = list(set(tmpArray))
-    #    tmpArray = sorted(tmpArray, key=natural_keys)
+            for line in lines:
+                cols = line.split()
+                if cols[-1] != "LISTEN":
+                    continue
+                if len(cols) >= 6:
+                    proc  = cols[1]  # Process name
+                    proto = cols[4]  # Protocol
+                    port  = cols[5]  # Port
+                    port = port.rsplit(':', -1)[-1]
+                    if port == "*":
+                        continue
+                    # Append the cleaned data
+                    connections.append((proc, proto, port))
 
-    #    self.editAllowedPorts.setText(' '.join(tmpArray))
-    #    return
+            # Sort connections by port
+            connections.sort(key=lambda x: int(x[2]))  # Sort by port
 
-def atoi(text):
-	return int(text) if text.isdigit() else text
+            return connections
 
-def natural_keys(text):
-	return [ atoi(c) for c in re.split('(\d+)',text) ]
+        except CalledProcessError as e:
+            print(f"Error retrieving listening ports: {e}")
+            return []
+
+    def natural_keys(self, text):
+        return [int(c) if c.isdigit() else c for c in re.split('(\d+)', text)]
 
 def main():
-    ipfwGUI = QApplication(sys.argv)
-
-    gui = simpleIpfwGui()
+    app = QApplication(sys.argv)
+    gui = SimpleIpfwGui()
     gui.show()
-    # Center main window
-    gui.setGeometry(QStyle.alignedRect(Qt.LayoutDirection. LeftToRight, Qt.AlignmentFlag.AlignCenter, gui.size(),
-        ipfwGUI.primaryScreen().geometry()))
-
-    sys.exit(ipfwGUI.exec())
-
+    gui.setGeometry(QStyle.alignedRect(Qt.LayoutDirection.LeftToRight, Qt.AlignmentFlag.AlignCenter, gui.size(), app.primaryScreen().geometry()))
+    sys.exit(app.exec())
 
 if __name__ == '__main__':
     main()
